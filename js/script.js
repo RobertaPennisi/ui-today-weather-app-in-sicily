b30eefb2d2eb6debf40da0cfa1eb4dad3fd1811a
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-11-30 12:17:58 +0100

    feature: added css style and bg images from js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import { showSelectedCity } from "./select.js";
 
 
 
+
 const loadCards = () => {
     APIendpoints.map( item =>
         fetch(item)
@@ -11,6 +12,16 @@ const loadCards = () => {
             .then(data => {
                 cities.push(data); 
                 createCardsList(cities);
+                
+                document.getElementById('2525763').classList.add('agrigento');
+                document.getElementById('2525068').classList.add('catania');
+                document.getElementById('2523918').classList.add('palermo');
+                document.getElementById('2523082').classList.add('siracusa');
+                document.getElementById('2525447').classList.add('caltanissetta');
+                document.getElementById('2524170').classList.add('messina');
+                document.getElementById('2524819').classList.add('enna');
+                document.getElementById('2523649').classList.add('ragusa');
+                document.getElementById('2522875').classList.add('trapani');
             })
     );
 };
@@ -18,6 +29,8 @@ const loadCards = () => {
 export const cities = [];
 const date = new Date();
 document.getElementById('today').textContent = date.toDateString();
+
+
 
 document.addEventListener("DOMContentLoaded", () =>{
     loadCards();
--- js/select.js
+++ js/select.js
@@ -13,7 +13,16 @@ export const showSelectedCity = (data) => {
         createCardsList(result);
         
         if (selectedCity === "all") {
-            createCardsList(cities);   
+            createCardsList(cities);
+            document.getElementById('2525763').classList.add('agrigento');
+            document.getElementById('2525068').classList.add('catania');
+            document.getElementById('2523918').classList.add('palermo');
+            document.getElementById('2523082').classList.add('siracusa');
+            document.getElementById('2525447').classList.add('caltanissetta');
+            document.getElementById('2524170').classList.add('messina');
+            document.getElementById('2524819').classList.add('enna');
+            document.getElementById('2523649').classList.add('ragusa');
+            document.getElementById('2522875').classList.add('trapani');   
         }
     });
 }
